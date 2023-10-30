4eafff2f91cf53155077b499874ebf3c3bd1b07a
shreyaparekh <shreyaparekh15@gmail.com>
2023-10-30 14:27:57 +0530

    commi30

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c='shreya';
+  render() {
+    return (
+      <div>
+        Hello my First classbaed component {this.c}
+      </div>
+    )
+  }
+}
+
+
