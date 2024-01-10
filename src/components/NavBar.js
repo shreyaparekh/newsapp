e0e5fd2bf336fd3d6999abbee1648107d1bccf06
shreyaparekh <shreyaparekh15@gmail.com>
2024-01-10 22:50:54 +0530

    10_01Pageinationwithroutinh

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import {
+  Link
+} from "react-router-dom";
 export class NavBar extends Component {
   static propTypes = {
 
@@ -11,23 +13,23 @@ export class NavBar extends Component {
       <div>
        <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">News Monkey</a>
+    <a className="navbar-brand" to="#">News Monkey</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="/">Home</a>
+          <Link className="nav-link" aria-current="page" to="/">Home</Link>
         </li>
-        <li className="nav-item"><a className="nav-link" href="/">About</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/business">Business</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/entertainment">Entertainment</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/general">General</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/health">Health</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/science">Science</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/sports">Sports</a> </li>
-        <li className="nav-item"><a className="nav-link" href="/technology">Technology</a> </li>
+        <li className="nav-item"><Link className="nav-link" to="/about">About</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/business">Business</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/general">General</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/health">Health</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/science">Science</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link> </li>
+        <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link> </li>
       </ul>
       
     </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
   render() {
-   let {title,description,imageUrl,newsurl}= this.props;
+    let { title, description, imageUrl, newsurl, author, date ,source} = this.props;
     return (
       <div>
         <div className="card">
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'90%'}}>
+                {source}
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            <h5 className="card-title">
+              {title}
+             
+            </h5>
             <p className="card-text">
-             {description}...
+              <p class="card-text">
+                <small class="text-muted">
+                  By {!author ? "Unknown" : author} on{" "}
+                  {new Date(date).toUTCString()}
+                </small>
+              </p>
+              {description}...
             </p>
-            <a rel="noreferrer"  href={newsurl} target="_blank"className="btn btn-sm btn-primary">
+            <a
+              rel="noreferrer"
+              href={newsurl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
               Read More
             </a>
           </div>
